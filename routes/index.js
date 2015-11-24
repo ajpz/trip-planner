db13b208d60fa1c72c3e6478a711085ae35e4c4c
ajpz <perez.jonthan@gmail.com>
2015-11-24 11:59:08 -0500

    Add all data find query

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var Promise = require('bluebird');
-var Models = require('./models');
-var Place = Models.Place;
+var Models = require('../models');
+// var Place = Models.Place;
 var Hotel = Models.Hotel;
 var Activity = Models.Activity;
 var Restaurant = Models.Restaurant;
 
 router.get('/', function(req, res, next) {
+	//kick off async db calls for all data
+	var hotels = Hotel.find({}).exec(), 
+		restaurants = Restaurant.find({}).exec(), 
+		activities = Activity.find({}).exec(); 
 
+	//user bluebird's "all" method to sync results
+	Promise.all([hotels, restaurants, activities])
+	.then(function(allResults) {
+		res.send(allResults); 
+	})	
 });
 
 
